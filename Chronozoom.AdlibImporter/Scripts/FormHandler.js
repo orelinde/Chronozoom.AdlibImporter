4753065aaf39c27d696b31fff402ff05eef3ad44
orelinde <richardje@live.no>
2015-05-31 20:00:53 +0200

    cleanup and added metadata

--- Chronozoom.AdlibImporter/Scripts/FormHandler.js
+++ Chronozoom.AdlibImporter/Scripts/FormHandler.js
@@ -5,13 +5,14 @@
     with the xml elements*/
     var getXmlElementsFromUrl = function () {
 
+        // Hide the input form
+        inputFormIsVisisble(false);
+        actionFormIsVisisble(false);
+        inputUrlPartIsLoading(true);
         /* The JSONP callback on success function which will scrape the xml elements
         from the received data. The method also checks if the returned data has the
         right properties to parse. if true then it will create a select list*/
         var callback = function (data) {
-            inputFormIsVisisble(true);
-            actionFormIsVisisble(true);
-            inputUrlPartIsLoading(false);
             if (!data.hasOwnProperty("recordList")) console.log("No recordList");
             if (!data.recordList.hasOwnProperty("record")) console.log("No recordslist found");
             if (!data.recordList.length === 0) console.log("No records found");
@@ -21,23 +22,37 @@
                     for (var attr in record['@attributes']) {
                         appendToDropdownlist(attr);
                     }
-                }
-                appendToDropdownlist(prop);
+                } 
             }
             setLists();
+            resetVisuals();
         }
 
-        // Hide the input form
-        inputFormIsVisisble(false);
-        actionFormIsVisisble(false);
-        var url = document.getElementById("url").value;
-        url = "http://am.adlibhosting.com/ChronoZoom/wwwopac.ashx?command=search&database=collect"; // TEMPORARY FOR DEBUG
+        var callbackMetadata = function (data) {
+            inputFormIsVisisble(true);
+            actionFormIsVisisble(true);
+            inputUrlPartIsLoading(false);
+            if (!data.hasOwnProperty("recordList")) console.log("No recordList");
+            if (!data.recordList.hasOwnProperty("record")) console.log("No recordslist found");
+            if (!data.recordList.length === 0) console.log("No records found");
+            var records = data.recordList.record;
+            for (var i = 0; i < records.length; i++) {
+                var obj = records[i];
+                appendToDropdownlist(obj.displayName[0].value[0]);
+            }
+            setLists();
+            resetVisuals();
+        }
+
+        var url = htmlElementValue("url");
+        var database = htmlElementValue("database");
+        url = "http://am.adlibhosting.com/ChronoZoom/"; // TEMPORARY FOR DEBUG
+        database = "collect";                           // TEMPORARY FOR DEBUG
 
         // Call api and make url part in loading state
-        inputUrlPartIsLoading(true);
-        Importer.Webhandler.GetXmlElementsFromUrl(url, callback);
-
-        var dropdownlistoptions = "";
+        Importer.Webhandler.GetXmlElementsFromUrl(url,database, callback);
+        Importer.Webhandler.GetMetadata(url, database, callbackMetadata);
+        
 
         // Set the options on the selectlist on the html
         function setLists() {
@@ -51,7 +66,20 @@
 
         }
 
+        /*Wait for all callbacks to complete before making the form visible again*/
+        var reset = 0;
+        function resetVisuals() {
+            reset = reset + 1;
+            if(reset ==2){
+            inputFormIsVisisble(true);
+            actionFormIsVisisble(true);
+            inputUrlPartIsLoading(false);
+            reset = 0;
+            }
+        }
+
         //Helper method to create an option per xml element
+        var dropdownlistoptions = "";
         function appendToDropdownlist(option) {
             dropdownlistoptions = dropdownlistoptions + "<option value=\"" + option + "\">" + option + "</option>";
         }
@@ -59,35 +87,37 @@
         // Append empty value and return the list with options
         function createSelectList() {
             var emptyOption = "<option value=\"null\"></option>";
-            return  emptyOption + dropdownlistoptions;
+            return emptyOption + dropdownlistoptions;
         }
+    }
 
+    function htmlElementValue(value) {
+        return document.getElementById(value).value;
+    }
+    function inputUrlPartIsLoading(isLoading) {
+        var urlInputForm = document.getElementById("urlInputLoader");
+        if (isLoading) {
+            urlInputForm.classList.add('active');
+        } else {
+            urlInputForm.classList.remove('active');
+        }
+    }
+    function inputFormIsVisisble(isVisible) {
+        var inputForm = document.getElementById("inputForm");
+        if (isVisible) {
+            inputForm.style.display = "block";
+        } else {
+            inputForm.style.display = "none";
+        }
+    }
+    function actionFormIsVisisble(isVisible) {
+        var inputForm = document.getElementById("actionForm");
+        if (isVisible) {
+            inputForm.style.display = "block";
+        } else {
+            inputForm.style.display = "none";
+        }
+    }
 
-        function inputUrlPartIsLoading(isLoading) {
-            var urlInputForm = document.getElementById("urlInputLoader");
-            if (isLoading) {
-                urlInputForm.classList.add('active');
-            } else {
-                urlInputForm.classList.remove('active');
-            }
-        }
-        function inputFormIsVisisble(isVisible) {
-            var inputForm = document.getElementById("inputForm");
-            if (isVisible) {
-                inputForm.style.display = "block";
-            } else {
-                inputForm.style.display = "none";
-            }
-        }
-        function actionFormIsVisisble(isVisible) {
-            var inputForm = document.getElementById("actionForm");
-            if (isVisible) {
-                inputForm.style.display = "block";
-            } else {
-                inputForm.style.display = "none";
-            }
-        }
-     }
-
-     Formhandler.GetXmlElementsFromUrl = getXmlElementsFromUrl;
+    Formhandler.GetXmlElementsFromUrl = getXmlElementsFromUrl;
 }(Importer.Formhandler = {}));
--- Chronozoom.AdlibImporter/Scripts/WebHandler.js
+++ Chronozoom.AdlibImporter/Scripts/WebHandler.js
@@ -2,9 +2,33 @@
 
     /* This method will call the Axiell api with the given url.
     It will try to get a record so that we can take te xml elements*/
-    var getXmlElementsFromUrl = function (url,callback) {
+    var getXmlElementsFromUrl = function (url,database,callback) {
         $.ajax({
-            url: url + '&output=json&limit=1&search=all&xmltype=unstructured',
+            url: url + 'wwwopac.ashx?&database=' + database + '&command=search&output=json&limit=1&search=all&xmltype=unstructured',
+            type: "GET",
+            dataType: "jsonp",
+            async: true,
+            cache: false,
+            contentType: "application/x-www-form-urlencoded",
+            data: 'json',
+            success: function (data) {
+                //Check the diagnostic node for errors
+                if (data.adlibJSON.diagnostic.error != undefined) {
+                    alert("error: " + data.adlibJSON.diagnostic.error.message);
+                }
+                else {
+                    callback(data.adlibJSON);
+                }
+            },
+            error: function (xhr, msg) {
+                alert("error: " + xhr.responseText + " " + msg);
+            }
+        });
+    }
+
+    var getMetadata = function (url,database, callback) {
+        $.ajax({
+            url: url + 'wwwopac.ashx?&database=' + database + '&output=json&command=getmetadata',
             type: "GET",
             dataType: "jsonp",
             async: true,
@@ -29,5 +53,6 @@
 
     // Public properties
     Webhandler.GetXmlElementsFromUrl = getXmlElementsFromUrl;
+    Webhandler.GetMetadata = getMetadata;
 
 }(Importer.Webhandler = {}));
