d5f50c7f1cfec6a566beae0f040f77f4ba334e63
orelinde <richardje@live.no>
2015-05-31 03:28:45 +0200

    added field and added list to field

--- Chronozoom.AdlibImporter/Scripts/FormHandler.js
+++ Chronozoom.AdlibImporter/Scripts/FormHandler.js
@@ -1,9 +1,10 @@
 ﻿(function (Formhandler) {
     var getXmlElementsFromUrl = function () {
         var url = document.getElementById("url").value;
-
+        url = "http://am.adlibhosting.com/ChronoZoom/wwwopac.ashx?command=search&database=collect";
+        var dropdownlistoptions = "";
         $.ajax({
-            url: url+'&output=json',
+            url: url + '&output=json&limit=1&search=all&xmltype=unstructured',
             type: "GET",
             dataType: "jsonp",
             async: true,
@@ -30,14 +31,37 @@
             if (!data.hasOwnProperty("recordList")) console.log("No recordList");
             if (!data.recordList.hasOwnProperty("record")) console.log("No recordslist found");
             if (!data.recordList.length === 0) console.log("No records found");
-            for (var prop in data.recordList.record[0]) {
-                console.log(prop);
+            var record = data.recordList.record[0];
+            for (var prop in record) {
+                if (prop === "@attributes") {
+                    for (var attr in record['@attributes']) {
+                        appendToDropdownlis(attr);
+                    }
+                }
+                appendToDropdownlis(prop);
             }
+            setLists();
         }
 
-        
+        function setLists() {
+            var list = createSelectList();
+            document.getElementById("title").innerHTML = list;
+            document.getElementById("description").innerHTML = list;
+            document.getElementById("begindate").innerHTML = list;
+            document.getElementById("enddate").innerHTML = list;
+            document.getElementById("images").innerHTML = list;
+            document.getElementById("id").innerHTML = list;
 
-       // Importer.Webhandler.GetXmlElementsFromUrl(url);
+        }
+
+        function appendToDropdownlis(option) {
+            dropdownlistoptions = dropdownlistoptions + "<option value=\"" + option + "\">" + option + "</option>";
+        }
+
+        function createSelectList() {
+            var emptyOption = "<option value=\"null\"></option>";
+            return "<select>"+ emptyOption + dropdownlistoptions + "</select>";
+        }
      }
 
      Formhandler.GetXmlElementsFromUrl = getXmlElementsFromUrl;
