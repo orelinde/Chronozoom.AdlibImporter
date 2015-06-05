2eb9c5b748b6ad0af3ce1f465f3a2f7201fca3c8
orelinde <richardje@live.no>
2015-06-05 09:15:11 +0200

    merge

---
+++ Chronozoom.AdlibImporter.Backend/Models/BatchCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Chronozoom.AdlibImporter.Backend.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class BatchCommand
+    {
+        [Required]
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "No Mappings found")]
+        public XmlMappings Mappings { get; set; }
+
+        [Required(ErrorMessage = "No Actions found")]
+        [MinLength(length:1,ErrorMessage = "At least one action must be present")]
+        public GroupAction[] Actions { get; set; }
+    }
+}
---
+++ Chronozoom.AdlibImporter.Backend/Models/XmlMappings.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Chronozoom.AdlibImporter.Backend.Models
+{
+    public class XmlMappings
+    {
+        [Required]
+        public string Title { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+        
+        [Required]
+        public string Begindate { get; set; }
+        
+        [Required]
+        public string Enddate { get; set; }
+        public string Images { get; set; }
+
+        [Required]
+        public string Id { get; set; }
+    }
+}
