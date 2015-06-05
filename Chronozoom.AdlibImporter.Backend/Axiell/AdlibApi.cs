6b863b4acead5b045f9af7a052af396eb56ab9e9
orelinde <richardje@live.no>
2015-06-06 01:37:37 +0200

    include

---
+++ Chronozoom.AdlibImporter.Backend/Axiell/AdlibApi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace Chronozoom.AdlibImporter.Backend.Axiell
+{
+    public class AdlibApi
+    {
+        public static AdlibFacetsRecords GetFacets(string url, string database, string facet)
+        {
+            var uri = String.Format("{0}/wwwopac.ashx?database={1}&command=facets&search=all&facet={2}&xmltype=unstructured&limit=1", url, database, facet);
+            using (var client = new HttpClient())
+            {
+                client.Timeout = Timeout.InfiniteTimeSpan;
+                string result = client.GetStringAsync(new Uri(uri)).Result;
+                XmlSerializer serializer = new XmlSerializer(typeof(AdlibFacetsRecords));
+                StringReader reader = new StringReader(result);
+                return (AdlibFacetsRecords)serializer.Deserialize(reader);
+                
+            }
+        }
+
+        public static List<string> GetContentItemsByFacets(string url, string database, string facet,AdlibFacetsRecords rootFacets)
+        {
+            using (var client = new HttpClient())
+            {
+                Parallel.ForEach(rootFacets.Records, rfacet =>
+                {
+                    var uri = String.Format("{0}/wwwopac.ashx?database={1}&search={2}={3}&xmltype=unstructured", url, database,facet, rfacet.Term);
+                    client.Timeout = Timeout.InfiniteTimeSpan;
+                    string result = client.GetStringAsync(new Uri(url)).Result;
+                    XmlDocument document = new XmlDocument();
+                    document.Load(new StringReader(result));
+
+                    var records = document.GetElementsByTagName("record");
+                    foreach (var record in records)
+                    {
+
+                    }
+                });
+            }
+            return null;
+        }
+    }
+}
---
+++ Chronozoom.AdlibImporter.Backend/Models/BatchCommand.cs
@@ -0,0 +1,43 @@
+﻿namespace Chronozoom.AdlibImporter.Backend.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class BatchCommand
+    {
+        /// <summary>
+        /// Timeline title
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// The description of the timeline
+        /// </summary>
+        public string Description { get; set; }
+        
+        /// <summary>
+        /// The mappings from adlibhosting database to Chronozoom format
+        /// </summary>
+        [Required(ErrorMessage = "No Mappings found")]
+        public XmlMappings Mappings { get; set; }
+
+        /// <summary>
+        /// The actions to apply to the data, these actions are group by actions
+        /// </summary>
+        [Required(ErrorMessage = "No Actions found")]
+        [MinLength(length:1,ErrorMessage = "At least one action must be present")]
+        public GroupAction[] Actions { get; set; }
+
+        /// <summary>
+        /// Adlibhosting url
+        /// </summary>
+ 
+        public string BaseUrl { get; set; }
+        
+        /// <summary>
+        /// Database of url ?database=
+        /// </summary>
+ 
+        public string Database { get; set; }
+    }
+}
